cd14dfa4d16f2797816ac7ea1d810cdd556e1706
Rifqi Naufal Tohari <rifqi.nufaltohari@gojek.com>
2023-12-27 21:20:11 +0700

    add more info to swagger & add Readme.md

--- src/main/kotlin/com/exercise/cards/controller/CardController.kt
+++ src/main/kotlin/com/exercise/cards/controller/CardController.kt
@@ -2,8 +2,15 @@ package com.exercise.cards.controller
 
 import com.exercise.cards.constant.CardConstant
 import com.exercise.cards.dto.Card
+import com.exercise.cards.dto.ErrorResponse
 import com.exercise.cards.dto.Response
 import com.exercise.cards.service.CardService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Pattern
 import org.springframework.http.HttpStatus
@@ -19,12 +26,34 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(
+    name = "CRUD REST APIs for Cards in X Bank",
+    description = "CRUD REST APIs in X Bank to CREATE, UPDATE, FETCH AND DELETE card details"
+)
 @RestController
 @RequestMapping(path = ["/api"], produces = [MediaType.APPLICATION_JSON_VALUE])
 @Validated
 class CardController(
     private val cardService: CardService
 ) {
+
+    @Operation(
+        summary = "Create Card REST API",
+        description = "REST API to create new Card inside EazyBank"
+    )
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status CREATED"
+        ),
+        ApiResponse(
+            responseCode = "500",
+            description = "HTTP Status Internal Server Error",
+            content = [Content(
+                schema = Schema(implementation = ErrorResponse::class)
+            )]
+        )
+    )
     @PostMapping("/create")
     fun create(
         @RequestParam
@@ -42,6 +71,25 @@ class CardController(
             )
     }
 
+    @Operation(
+        summary = "Fetch Card Details REST API",
+        description = "REST API to fetch card details based on a mobile number"
+    )
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+        ),
+        ApiResponse(
+            responseCode = "500",
+            description = "HTTP Status Internal Server Error",
+            content = [Content(
+                schema = Schema(
+                    implementation = ErrorResponse::class
+                )
+            )]
+        )
+    )
     @GetMapping("/fetch")
     fun fetch(
         @RequestParam
@@ -54,6 +102,29 @@ class CardController(
             .body(loan)
     }
 
+    @Operation(
+        summary = "Update Card Details REST API",
+        description = "REST API to update card details based on a card number"
+    )
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+        ),
+        ApiResponse(
+            responseCode = "417",
+            description = "Expectation Failed"
+        ),
+        ApiResponse(
+            responseCode = "500",
+            description = "HTTP Status Internal Server Error",
+            content = [Content(
+                schema = Schema(
+                    implementation = ErrorResponse::class
+                )
+            )]
+        )
+    )
     @PutMapping("/update")
     fun update(@RequestBody @Valid card: Card): ResponseEntity<Response> {
         val isUpdated = cardService.updateCard(card)
@@ -79,6 +150,29 @@ class CardController(
         }
     }
 
+    @Operation(
+        summary = "Delete Card Details REST API",
+        description = "REST API to delete Card details based on a mobile number"
+    )
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+        ),
+        ApiResponse(
+            responseCode = "417",
+            description = "Expectation Failed"
+        ),
+        ApiResponse(
+            responseCode = "500",
+            description = "HTTP Status Internal Server Error",
+            content = [Content(
+                schema = Schema(
+                    implementation = ErrorResponse::class
+                )
+            )]
+        )
+    )
     @DeleteMapping("/delete")
     fun delete(
         @RequestParam
