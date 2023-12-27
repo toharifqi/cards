9fa11d5fc94993160c4044dd8295fab1083bd5ec
Rifqi Naufal Tohari <rifqinaufaltohari@gmail.com>
2023-12-27 19:12:58 +0700

    Initial Commit

---
+++ src/main/kotlin/com/exercise/cards/CardsApplication.kt
@@ -0,0 +1,11 @@
+package com.exercise.cards
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CardsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CardsApplication>(*args)
+}
