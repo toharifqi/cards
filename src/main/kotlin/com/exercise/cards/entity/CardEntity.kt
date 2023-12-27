96b9c8ebc683d04a04d7928dab57bd29c39fe53b
Rifqi Naufal Tohari <rifqi.nufaltohari@gojek.com>
2023-12-27 20:35:57 +0700

    add validation and schema to DTO

--- src/main/kotlin/com/exercise/cards/entity/CardEntity.kt
+++ src/main/kotlin/com/exercise/cards/entity/CardEntity.kt
@@ -6,6 +6,10 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import org.hibernate.annotations.GenericGenerator
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
@@ -29,12 +33,16 @@ data class CardEntity(
 
     val availableAmount: Int,
 
+    @CreatedDate
     override var createdAt: LocalDateTime? = null,
 
+    @CreatedBy
     override var createdBy: String? = null,
 
+    @LastModifiedDate
     override var updatedAt: LocalDateTime? = null,
 
+    @LastModifiedBy
     override var updatedBy: String? = null,
 
 ) : BaseEntity
